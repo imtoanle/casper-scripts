17ce2a0e199138696577dc622b7c3f99ef032b0e
Toan Le <ktoanlba@gmail.com>
2021-08-18 15:33:18 +0700

    update

--- ip_watcher.sh
+++ ip_watcher.sh
@@ -7,7 +7,7 @@ public_ip="$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '"')
 if [[ ! -z "$public_ip" ]] && [ -z "$(grep -rnw /etc/casper/*/config.toml -e "public_address = '$public_ip:0'")" ]
 then
   fdfind config.toml /etc/casper/ -X sudo sed -i -E "s/public_address = '.+'/public_address = '$public_ip:0'/g"
-  echo $(date +"%d/%m/%Y - %T"): Updated new IP $(public_ip) >> ./logs/ip_watcher.log
+  echo $(date +"%d/%m/%Y - %T"): Updated new IP $public_ip >> ./logs/ip_watcher.log
 fi
 
 sudo systemctl stop casper-node-launcher; sleep 2
