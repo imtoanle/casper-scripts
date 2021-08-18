58816d43d7fd628fb44df00b69df6197845d233f
Toan Le <ktoanlba@gmail.com>
2021-08-18 21:25:06 +0700

    fix bug

--- ip_watcher.sh
+++ ip_watcher.sh
@@ -8,8 +8,8 @@ if [[ ! -z "$public_ip" ]] && [ -z "$(grep -rnw /etc/casper/*/config.toml -e "pu
 then
   fdfind config.toml /etc/casper/ -X sudo sed -i -E "s/public_address = '.+'/public_address = '$public_ip:0'/g"
   echo $(date +"%d/%m/%Y - %T"): Updated new IP $public_ip >> ./logs/ip_watcher.log
+  sudo systemctl stop casper-node-launcher; sleep 2
+  sudo logrotate -f /etc/logrotate.d/casper-node
+  sudo systemctl start casper-node-launcher
 fi
 
-sudo systemctl stop casper-node-launcher; sleep 2
-sudo logrotate -f /etc/logrotate.d/casper-node
-sudo systemctl start casper-node-launcher
